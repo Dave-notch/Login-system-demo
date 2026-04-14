5b6220e01168498200c6bbac1bc776c11d72a09c
dave-notch <stsaamir86@gmail.com>
2026-04-14 17:12:33 +0300

    ref: route fix

--- app.js
+++ app.js
@@ -140,3 +140,7 @@ app.use((err, req, res, next)=>{
 app.listen(8000, () => {
   console.log('Server running on port 8000');
 });
+
+app.get("/", (req, res) => {
+  res.send("Server is running")
+})
